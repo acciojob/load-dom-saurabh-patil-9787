fa596b6a0c68769ead6c3562da4e0aa162ff1bda
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-05 14:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+        document.addEventListener("DOMContentLoaded",()=>{
+        let msg = document.createTextNode("DOM load success")
+
+        document.body.appendChild(msg)
+
+        })
